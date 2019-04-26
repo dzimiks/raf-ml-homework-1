02c61801fa07b56c0cc197dff8b6fadf5176bbb9
branko_fu <brankofulurija@gmail.com>
2019-04-26 18:11:18 +0200

    Finished 2a problem

--- 2a.py
+++ 2a.py
@@ -3,112 +3,115 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# Pomocna funkcija koja od niza trening primera pravi feature matricu (m X n).
 def create_feature_matrix(x, nb_features):
-	"""
-	An auxiliary function that creates feature matrix (M x N) from a series of training examples.
-
-	:param x: Array of numbers.
-	:param nb_features: Degree of a polynomial.
-	:return: The array formed by stacking the given arrays.
-	"""
-
-	tmp_features = []
-
-	for deg in range(1, nb_features + 1):
-		tmp_features.append(np.power(x, deg))
-
-	return np.column_stack(tmp_features)
+    tmp_features = []
+    for deg in range(1, nb_features + 1):
+        tmp_features.append(np.power(x, deg))
+    return np.column_stack(tmp_features)
 
 
-tf.reset_default_graph()
+def polynomial_regression(input_data, nb_samples, nb_features, color):
+    # Restartuj graf
+    tf.reset_default_graph()
 
-# Avoid a scientific notation and round off on 5 decimals
-np.set_printoptions(suppress=True, precision=5)
+    # Kreiranje feature matrice.
+    data = input_data.copy()
+    data['x'] = create_feature_matrix(data['x'], nb_features)
 
-# Step 1: Loading and processing data
-filename = 'data/funky.csv'
-all_data = np.loadtxt(filename, delimiter=',')
-data = dict()
-data['x'] = all_data[:, 0]
-data['y'] = all_data[:, 1]
+    # Korak 2: Model.
+    X = tf.placeholder(shape=(None, nb_features), dtype=tf.float32)
+    Y = tf.placeholder(shape=(None), dtype=tf.float32)
+    w = tf.Variable(tf.zeros(nb_features))
+    bias = tf.Variable(0.0)
 
-# Random shuffle
-nb_samples = data['x'].shape[0]
-indices = np.random.permutation(nb_samples)
-data['x'] = data['x'][indices]
-data['y'] = data['y'][indices]
+    w_col = tf.reshape(w, (nb_features, 1))
+    hyp = tf.add(tf.matmul(X, w_col), bias)
 
-# Normalization
-data['x'] = (data['x'] - np.mean(data['x'], axis=0)) / np.std(data['x'], axis=0)
-data['y'] = (data['y'] - np.mean(data['y'])) / np.std(data['y'])
+    # Korak 3: Funkcija troška i optimizacija.
+    Y_col = tf.reshape(Y, (-1, 1))
+    loss = tf.reduce_mean(tf.square(hyp - Y_col))
 
-# Feature matrix creation
-# This variable controls the number of features, ie. degree of polynomial
-nb_features = 6
-print('Original values (first 3):')
-print(data['x'][:3])
-print('Feature matrix (first 3 rows):')
-data['x'] = create_feature_matrix(data['x'], nb_features)
-print(data['x'][:3, :])
+    # Prelazimo na AdamOptimizer jer se prost GradientDescent lose snalazi sa
+    # slozenijim funkcijama.
+    opt_op = tf.train.AdamOptimizer().minimize(loss)
 
-# Plotting all data
-# plt.scatter(data['x'][:, 0], data['y'])
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.show()
+    # Korak 4: Trening.
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-# Step 2: Model.
-X = tf.placeholder(shape=(None, nb_features), dtype=tf.float32)
-Y = tf.placeholder(shape=None, dtype=tf.float32)
-w = tf.Variable(tf.zeros(nb_features))
-bias = tf.Variable(0.0)
+        # Izvršavamo 100 epoha treninga.
+        nb_epochs = 100
+        for epoch in range(nb_epochs):
 
-w_col = tf.reshape(w, (nb_features, 1))
-hyp = tf.add(tf.matmul(X, w_col), bias)
+            # Stochastic Gradient Descent.
+            epoch_loss = 0
+            for sample in range(nb_samples):
+                feed = {X: data['x'][sample].reshape((1, nb_features)),
+                        Y: data['y'][sample]}
+                _, curr_loss = sess.run([opt_op, loss], feed_dict=feed)
+                epoch_loss += curr_loss
 
-# Step 3: Cost function and optimization
-Y_col = tf.reshape(Y, (-1, 1))
-loss = tf.reduce_mean(tf.square(hyp - Y_col))
+            # U svakoj desetoj epohi ispisujemo prosečan loss.
+            epoch_loss /= nb_samples
+            if (epoch + 1) % 10 == 0:
+                print('Epoch: {}/{}| Avg loss: {:.5f}'.format(epoch + 1, nb_epochs,
+                                                              epoch_loss))
 
-# Switching to AdamOptimizer because the GradientDescent loses with more complex functions
-opt_op = tf.train.AdamOptimizer().minimize(loss)
+        # Ispisujemo i plotujemo finalnu vrednost parametara.
+        w_val = sess.run(w)
+        bias_val = sess.run(bias)
+        print('w = ', w_val, 'bias = ', bias_val)
+        xs = create_feature_matrix(np.linspace(-2, 4, 100), nb_features)
+        hyp_val = sess.run(hyp, feed_dict={X: xs})  # Bez Y jer nije potrebno.
+        plt.plot(xs[:, 0].tolist(), hyp_val.tolist(), color=color)
+        plt.xlim([-2, 2])
+        plt.ylim([-3, 4])
 
-# Step 4: Training
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
+        return sess.run(loss, feed_dict={X: data['x'], Y: data['y']})
 
-	# 1000 epochs of training
-	nb_epochs = 1000
 
-	for epoch in range(nb_epochs):
-		# Stochastic Gradient Descent.
-		epoch_loss = 0
+def main():
+    # Izbegavamo scientific notaciju i zaokruzujemo na 5 decimala.
+    np.set_printoptions(suppress=True, precision=5)
 
-		for sample in range(nb_samples):
-			feed = {X: data['x'][sample].reshape((1, nb_features)),
-					Y: data['y'][sample]}
-			_, curr_loss = sess.run([opt_op, loss], feed_dict=feed)
-			epoch_loss += curr_loss
+    # Korak 1: Učitavanje i obrada podataka.
+    filename = 'data/funky.csv'
+    all_data = np.loadtxt(filename, delimiter=',', skiprows=0, usecols=(0, 1))
+    data = dict()
+    data['x'] = all_data[:, 0]
+    data['y'] = all_data[:, 1]
 
-			if epoch == nb_epochs - 1 and data['x'][sample][0] == 3:
-				print(data['x'][sample])
-				print(curr_loss)
+    # Nasumično mešanje.
+    nb_samples = data['x'].shape[0]
+    indices = np.random.permutation(nb_samples)
+    data['x'] = data['x'][indices]
+    data['y'] = data['y'][indices]
 
-		# Writes average loss in every hundred epoch
-		epoch_loss /= nb_samples
+    # Normalizacija (obratiti pažnju na axis=0).
+    data['x'] = (data['x'] - np.mean(data['x'], axis=0)) / np.std(data['x'], axis=0)
+    data['y'] = (data['y'] - np.mean(data['y'])) / np.std(data['y'])
 
-		if (epoch + 1) % 100 == 0:
-			print('Epoch: {}/{} | Avg loss: {:.5f}'.format(epoch + 1, nb_epochs, epoch_loss))
+    # Iscrtavanje podataka
+    plt.scatter(data['x'], data['y'])
 
-	# Print and plot the final value of the parameters
-	w_val = sess.run(w)
-	bias_val = sess.run(bias)
-	print('w = ', w_val, 'bias = ', bias_val)
-	xs = create_feature_matrix(np.linspace(-2, 4, 100), nb_features)
-	hyp_val = sess.run(hyp, feed_dict={X: xs})
-	plt.plot(xs[:, 0].tolist(), hyp_val.tolist(), color='g')
-	plt.scatter(data['x'][:, 0], data['y'])
-	plt.xlim([-2, 2])
-	plt.ylim([-3, 3.5])
-	plt.title('Features ' + str(nb_features))
-	plt.show()
+    colors = ['g', 'm', 'b', 'r', 'k', 'y']
+    losses = []
+
+    for degree in range(1, 7):
+        print('Polynomial regression for degree = ', degree)
+        loss = polynomial_regression(data, nb_samples, degree, colors[degree - 1])
+        losses.append(loss)
+        print('loss = {:.5f}'.format(loss))
+        print('----------------------------')
+
+    plt.show()
+
+    plt.plot(range(1, 7), losses, color='g')
+    plt.xlabel("Degree")
+    plt.ylabel("Loss")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
