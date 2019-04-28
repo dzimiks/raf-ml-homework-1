27040988c80f56e9965c5a7f583e281491a6ef5d
branko_fu <brankofulurija@gmail.com>
2019-04-29 01:16:22 +0200

    Poceo naive bayes

--- 2b.py
+++ 2b.py
@@ -1,1 +1,119 @@
-# TODO popuniti kodom za problem 2b
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Pomocna funkcija koja od niza trening primera pravi feature matricu (m X n).
+def create_feature_matrix(x, nb_features):
+    tmp_features = []
+    for deg in range(1, nb_features + 1):
+        tmp_features.append(np.power(x, deg))
+    return np.column_stack(tmp_features)
+
+
+def polynomial_regression(input_data, nb_samples, nb_features, color):
+    # Restartuj graf
+    tf.reset_default_graph()
+
+    # Kreiranje feature matrice.
+    data = input_data.copy()
+    data['x'] = create_feature_matrix(data['x'], nb_features)
+
+    # Korak 2: Model.
+    X = tf.placeholder(shape=(None, nb_features), dtype=tf.float32)
+    Y = tf.placeholder(shape=(None), dtype=tf.float32)
+    w = tf.Variable(tf.zeros(nb_features))
+    bias = tf.Variable(0.0)
+
+    w_col = tf.reshape(w, (nb_features, 1))
+    hyp = tf.add(tf.matmul(X, w_col), bias)
+
+    # Korak 3: Funkcija troška i optimizacija.
+    Y_col = tf.reshape(Y, (-1, 1))
+    l2 = tf.multiply(0.01, tf.nn.l2_loss(w))
+    loss = tf.add(tf.reduce_mean(tf.square(hyp - Y_col)), 0.01 * tf.nn.l2_loss(w))
+
+    # Prelazimo na AdamOptimizer jer se prost GradientDescent lose snalazi sa
+    # slozenijim funkcijama.
+    opt_op = tf.train.AdamOptimizer().minimize(loss)
+
+    # Korak 4: Trening.
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        # Izvršavamo 100 epoha treninga.
+        nb_epochs = 100
+        for epoch in range(nb_epochs):
+
+            # Stochastic Gradient Descent.
+            epoch_loss = 0
+            for sample in range(nb_samples):
+                feed = {X: data['x'][sample].reshape((1, nb_features)),
+                        Y: data['y'][sample]}
+                _, curr_loss, l2_loss = sess.run([opt_op, loss, l2], feed_dict=feed)
+                epoch_loss += curr_loss
+                print(l2_loss)
+
+            # U svakoj desetoj epohi ispisujemo prosečan loss.
+            epoch_loss /= nb_samples
+            if (epoch + 1) % 10 == 0:
+                print('Epoch: {}/{}| Avg loss: {:.5f}'.format(epoch + 1, nb_epochs,
+                                                              epoch_loss))
+
+        # Ispisujemo i plotujemo finalnu vrednost parametara.
+        w_val = sess.run(w)
+        bias_val = sess.run(bias)
+        print('w = ', w_val, 'bias = ', bias_val)
+        xs = create_feature_matrix(np.linspace(-2, 4, 100), nb_features)
+        hyp_val = sess.run(hyp, feed_dict={X: xs})  # Bez Y jer nije potrebno.
+        plt.plot(xs[:, 0].tolist(), hyp_val.tolist(), color=color)
+        plt.xlim([-2, 2])
+        plt.ylim([-3, 4])
+
+        return sess.run(loss, feed_dict={X: data['x'], Y: data['y']})
+
+
+def main():
+    # Izbegavamo scientific notaciju i zaokruzujemo na 5 decimala.
+    np.set_printoptions(suppress=True, precision=5)
+
+    # Korak 1: Učitavanje i obrada podataka.
+    filename = 'data/funky.csv'
+    all_data = np.loadtxt(filename, delimiter=',', skiprows=0, usecols=(0, 1))
+    data = dict()
+    data['x'] = all_data[:, 0]
+    data['y'] = all_data[:, 1]
+
+    # Nasumično mešanje.
+    nb_samples = data['x'].shape[0]
+    indices = np.random.permutation(nb_samples)
+    data['x'] = data['x'][indices]
+    data['y'] = data['y'][indices]
+
+    # Normalizacija (obratiti pažnju na axis=0).
+    data['x'] = (data['x'] - np.mean(data['x'], axis=0)) / np.std(data['x'], axis=0)
+    data['y'] = (data['y'] - np.mean(data['y'])) / np.std(data['y'])
+
+    # Iscrtavanje podataka
+    plt.scatter(data['x'], data['y'])
+
+    colors = ['g', 'm', 'b', 'r', 'k', 'y']
+    losses = []
+
+    for degree in range(3, 4):
+        print('Polynomial regression for degree = ', degree)
+        loss = polynomial_regression(data, nb_samples, degree, colors[degree - 1])
+        losses.append(loss)
+        print('loss = {:.5f}'.format(loss))
+        print('----------------------------')
+
+    plt.show()
+
+    plt.plot(range(3, 4), losses, color='g')
+    plt.xlabel("Degree")
+    plt.ylabel("Loss")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- 4.py
+++ 4.py
@@ -1,153 +1,217 @@
 from nltk import FreqDist
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize, regexp_tokenize
-from nltk.stem import PorterStemmer
+from nltk.tokenize import wordpunct_tokenize
 from string import punctuation
-
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 import numpy as np
 import math
 import os
 
-porter = PorterStemmer()
-corpus = ''
-
-for root, _, files in os.walk('data/imdb'):
-	for name in files:
-		file_path = os.path.join(root, name)
-		# print('File:', file_path)
-
-		with open(file_path, 'r') as f:
-			file_data = f.read()
-			corpus += file_data
-
-# Cleaning the corpus
-print('Cleaning the corpus...')
-clean_corpus = []
-stop_punc = set(stopwords.words('english')).union(set(punctuation))
-corpus = sent_tokenize(corpus)
-
-for doc in corpus[:5]:
-	# words = wordpunct_tokenize(doc)
-	words = regexp_tokenize(doc, "[\w']+")
-	words_lower = [w.lower() for w in words]
-	print('words_lower:', words_lower)
-	words_filtered = [w for w in words_lower if w not in stop_punc]
-	print('words_filtered:', words_filtered)
-	words_stemmed = [porter.stem(w) for w in words_filtered]
-	print('words_stemmed:', words_stemmed)
-	clean_corpus.append(words_stemmed)
-	print()
-
-# Create vocabulary
-print('Creating the vocabulary...')
-vocab_set = set()
-
-for doc in clean_corpus:
-	for word in doc:
-		vocab_set.add(word)
-
-vocab = list(vocab_set)
-vocabulary = list(zip(vocab, range(len(vocab))))
-
-print('Vocabulary:', vocabulary)
-print('Feature vector size: ', len(vocab))
+VOCAB_SIZE = 1000
 
 
-# with open('out.txt', 'w') as f:
-# 	for w in vocabulary:
-# 		f.write(':'.join(map(str, w)) + '\n')
+class MultinomialNaiveBayes:
+    def __init__(self, nb_classes, nb_words, pseudocount):
+        self.nb_classes = nb_classes
+        self.nb_words = nb_words
+        self.pseudocount = pseudocount
 
+    def fit(self, X, Y):
+        nb_examples = X.shape[0]
 
-def occ_score(word, doc):
-	return 1 if word in doc else 0
+        # Racunamo P(Klasa) - priors
+        # np.bincount nam za datu listu vraca broj pojavljivanja svakog celog
+        # broja u intervalu [0, maksimalni broj u listi]
+        self.priors = np.bincount(Y) / nb_examples
+        print('Priors:')
+        print(self.priors)
+
+        # Racunamo broj pojavljivanja svake reci u svakoj klasi
+        occs = np.zeros((self.nb_classes, self.nb_words))
+        for i in range(nb_examples):
+            c = Y[i]
+            for w in range(self.nb_words):
+                cnt = X[i][w]
+                occs[c][w] += cnt
+        print('Occurences:')
+        print(occs)
+
+        # Racunamo P(Rec_i|Klasa) - likelihoods
+        self.like = np.zeros((self.nb_classes, self.nb_words))
+        for c in range(self.nb_classes):
+            for w in range(self.nb_words):
+                up = occs[c][w] + self.pseudocount
+                down = np.sum(occs[c]) + self.nb_words * self.pseudocount
+                self.like[c][w] = up / down
+        print('Likelihoods:')
+        print(self.like)
+
+    def predict(self, bow):
+        # Racunamo P(Klasa|bow) za svaku klasu
+        probs = np.zeros(self.nb_classes)
+        for c in range(self.nb_classes):
+            prob = np.log(self.priors[c])
+            for w in range(self.nb_words):
+                cnt = bow[w]
+                prob += cnt * np.log(self.like[c][w])
+            probs[c] = prob
+        # Trazimo klasu sa najvecom verovatnocom
+        print('\"Probabilites\" for a test BoW (with log):')
+        print(probs)
+        prediction = np.argmax(probs)
+        return prediction
+
+    def predict_multiply(self, bow):
+        # Racunamo P(Klasa|bow) za svaku klasu
+        # Mnozimo i stepenujemo kako bismo uporedili rezultate sa slajdovima
+        probs = np.zeros(self.nb_classes)
+        for c in range(self.nb_classes):
+            prob = self.priors[c]
+            for w in range(self.nb_words):
+                cnt = bow[w]
+                prob *= self.like[c][w] ** cnt
+            probs[c] = prob
+        # Trazimo klasu sa najvecom verovatnocom
+        # print('\"Probabilities\" for a test BoW (without log):')
+        # print(probs)
+        prediction = np.argmax(probs)
+        return prediction
 
 
 def numocc_score(word, doc):
-	return doc.count(word)
+    return doc.count(word)
 
 
-def freq_score(word, doc):
-	return doc.count(word) / len(doc) if len(doc) > 0 else 0
+def clean_data(corpus, labels):
+    # Priprema podataka
+    porter = PorterStemmer()
+
+    # Cistimo korpus
+    print('Cleaning the corpus...')
+    clean_corpus = []
+    stop_punc = set(stopwords.words('english')).union(set(punctuation)).union({'br', '...'})
+    for doc in corpus:
+        words = wordpunct_tokenize(doc)
+        words_lower = [w.lower() for w in words]
+        words_filtered = [w for w in words_lower if w not in stop_punc and '<' not in w and '>' not in w and ')' not in w and '(' not in w]
+        words_stemmed = [porter.stem(w) for w in words_filtered]
+        # print('Final:', words_stemmed)
+        clean_corpus.append(words_stemmed)
+    # Primetiti razliku u vokabularu kada se koraci izostave
+
+    # Kreiramo vokabular
+    print('Creating the vocab...')
+    vocab_dict = dict()
+    #   vocab_set = set()
+    for doc in clean_corpus:
+        for word in doc:
+            #       vocab_set.add(word)
+            vocab_dict.setdefault(word, 0)
+            vocab_dict[word] += 1
+
+    #   vocab = list(vocab_set)
+    vocab = sorted(vocab_dict, key=vocab_dict.get, reverse=True)[:VOCAB_SIZE]
+
+    print('Vocab:', list(zip(vocab, range(len(vocab)))))
+    print('Feature vector size: ', len(vocab))
+
+    # 1: Bag of Words model
+    print('Creating BOW features...')
+    X = np.zeros((len(clean_corpus), len(vocab)), dtype=np.float32)
+    for doc_idx in range(len(clean_corpus)):
+        doc = clean_corpus[doc_idx]
+        X[doc_idx] = create_bow(doc, vocab)
+        # for word_idx in range(len(vocab)):
+        #     word = vocab[word_idx]
+        #     cnt = numocc_score(word, doc)
+        #     X[doc_idx][word_idx] = cnt
+
+    Y = np.zeros(len(clean_corpus), dtype=np.int8)
+    for i in range(len(Y)):
+        Y[i] = labels[i]
+
+    return X, Y, vocab
 
 
-def get_bow():
-	# 1: Bag of Words model sa 3 različita scoringa
-	np.set_printoptions(precision=2, linewidth=200)
-	print('Creating BOW features...')
-
-	for score_fn in [occ_score, numocc_score, freq_score]:
-		X = np.zeros((len(clean_corpus), len(vocab)), dtype=np.float32)
-
-		for doc_idx in range(len(clean_corpus)):
-			doc = clean_corpus[doc_idx]
-
-			for word_idx in range(len(vocab)):
-				word = vocab[word_idx]
-				cnt = score_fn(word, doc)
-				X[doc_idx][word_idx] = cnt
-
-		print('X:')
-		print(X)
-		print()
+def create_bow(doc, vocab):
+    bow = np.zeros(len(vocab), dtype=np.float32)
+    for word_idx in range(len(vocab)):
+        word = vocab[word_idx]
+        cnt = numocc_score(word, doc)
+        bow[word_idx] = cnt
+    return bow
 
 
-def get_bigrams():
-	# 2. Bigram model
-	for doc in clean_corpus:
-		bigrams = []
+def read_data():
+    corpus = []
+    labels = []
+    print('Reading data...')
+    for root, _, files in os.walk('./data/imdb/'):
+        for name in files:
+            file_path = os.path.join(root, name)
+            doc = ''
+            with open(file_path, 'r', encoding='utf8') as f:
+                file_data = f.read()
+                doc += file_data
 
-		for i in range(len(doc) - 1):
-			bigram = doc[i] + ' ' + doc[i + 1]
-			bigrams.append(bigram)
+            corpus.append(doc)
+            label = 1 if 'pos' in file_path else 0
+            labels.append(label)
 
-		print('Bigrams:', bigrams)
+    indices = np.random.permutation(len(corpus))
+    corpus = np.asarray(corpus)
+    corpus = corpus[indices]
+    labels = np.asarray(labels)
+    labels = labels[indices]
 
-
-def get_tf_idf():
-	# 3. TF-IDF
-	# Racunamo IDF mapu
-	print('Calculating the IDF table...')
-	doc_counts = dict()
-
-	for word in vocab:
-		doc_counts[word] = 0
-
-		for doc in clean_corpus:
-			if word in doc:
-				doc_counts[word] += 1
-
-	print('Doc counts:')
-	print(doc_counts)
-	idf_table = dict()
-
-	for word in vocab:
-		idf = math.log10(len(corpus) / doc_counts[word])
-		idf_table[word] = idf
-
-	print('IDF table:')
-	print(idf_table)
-
-	# Isti kod kao kod BOW
-	def tfidf_score(word, doc):
-		tf = freq_score(word, doc)
-		idf = idf_table[word]
-		return tf * idf
-
-	print('Creating TF-IDF features...')
-	X = np.zeros((len(clean_corpus), len(vocab)), dtype=np.float32)
-
-	for doc_idx in range(len(clean_corpus)):
-		doc = clean_corpus[doc_idx]
-		for word_idx in range(len(vocab)):
-			word = vocab[word_idx]
-			cnt = tfidf_score(word, doc)
-			X[doc_idx][word_idx] = cnt
-
-	print('X:')
-	print(X)
+    return corpus, labels
 
 
-# TODO: MODELOVANJE I SIMULACIJA
-get_bow()
-get_bigrams()
-get_tf_idf()
+def main():
+    np.set_printoptions(precision=12, linewidth=200)
+
+    corpus, labels = read_data()
+    print('corpus len = ', len(corpus))
+
+    limit = math.floor(len(corpus) * 0.80)
+    train_corpus = corpus[:limit]
+    train_labels = labels[:limit]
+
+    test_corpus = corpus[limit:]
+    test_labels = labels[limit:]
+
+    print(len(train_corpus), len(train_labels))
+    print(len(test_corpus), len(test_labels))
+
+    X, Y, vocab = clean_data(train_corpus, train_labels)
+    print(vocab)
+    print()
+    print(X)
+    print(Y)
+    print(len(Y))
+    print()
+
+    class_names = ['Negative', 'Positive']
+    model = MultinomialNaiveBayes(nb_classes=2, nb_words=VOCAB_SIZE, pseudocount=1)
+    model.fit(X, Y)
+
+    success = 0
+    print()
+    for i in range(len(test_corpus)):
+        doc = test_corpus[i]
+        label = test_labels[i]
+        bow = create_bow(doc, vocab)
+        # print(bow)
+        # bow = np.zeros(VOCAB_SIZE)
+
+        prediction = model.predict(bow)
+        print('Predicted class (with log): ', class_names[prediction], class_names[test_labels[i]])
+        if prediction == test_labels[i]:
+            success += 1
+        # prediction = model.predict_multiply(test_bow)
+        # print('Predicted class (without log): ', class_names[prediction])
+
+    print(success / len(test_corpus))
+
+if __name__ == '__main__':
+    main()
